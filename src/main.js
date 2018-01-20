2d1f584991425ca35602c425306a4ed722310327
Zack <hickmanzackary@gmail.com>
2018-01-20 15:32:49 -0600

    updated py proc

--- src/main.js
+++ src/main.js
@@ -33,15 +33,40 @@ const selectPort = () => {
   return pyPort
 }
 
-const createPyProc = () => {
-  let port = '' + selectPort()
-  let script = path.join(__dirname, 'pyProcess', 'process.py')
-  pyProc = require('child_process').spawn('python', [script, port])
-  if (pyProc != null) {
-    console.log('child process success')
-  }
+const PY_DIST_FOLDER = 'pycalcdist'
+const PY_FOLDER = 'pycalc'
+const PY_MODULE = 'api' // without .py suffix
+
+const guessPackaged = () => {
+  const fullPath = path.join(__dirname, PY_DIST_FOLDER)
+  return require('fs').existsSync(fullPath)
 }
 
+const getScriptPath = () => {
+  if (!guessPackaged()) {
+    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
+  }
+  if (process.platform === 'win32') {
+    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + '.exe')
+  }
+  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE)
+}
+
+const createPyProc = () => {
+  let script = getScriptPath()
+  let port = '' + selectPort()
+
+  if (guessPackaged()) {
+    pyProc = require('child_process').execFile(script, [port])
+  } else {
+    pyProc = require('child_process').spawn('python', [script, port])
+  }
+
+  if (pyProc != null) {
+    //console.log(pyProc)
+    console.log('child process success on port ' + port)
+  }
+}
 const exitPyProc = () => {
   pyProc.kill()
   pyProc = null
