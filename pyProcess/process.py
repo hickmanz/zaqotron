1f76d9036cf32fe739635d47fb4c602acafe1df3
U-OPTIMUSPRIME\hickm <zhickman@solarintechnologies.com>
2018-01-21 18:38:22 -0600

    ix this

--- pyProcess/process.py
+++ pyProcess/process.py
@@ -40,58 +40,23 @@ def index():
         thread = sio.start_background_task(background_thread)
     return render_template('index.html')
 
-
-@sio.on('my event', namespace='/test')
-def test_message(sid, message):
-    sio.emit('my response', {'data': message['data']}, room=sid,
-             namespace='/test')
-
+@sio.on('start-process', namespace='/proc')
+def addData(sid, message):
+    sio.emit('logMessage', {'data': 'Data recieved from client'}, namespace='/test')
+    #update variables
+    processData(sid)
 
 @sio.on('my broadcast event', namespace='/test')
 def test_broadcast_message(sid, message):
     sio.emit('my response', {'data': message['data']}, namespace='/test')
 
-
-@sio.on('join', namespace='/test')
-def join(sid, message):
-    sio.enter_room(sid, message['room'], namespace='/test')
-    sio.emit('my response', {'data': 'Entered room: ' + message['room']},
-             room=sid, namespace='/test')
-
-
-@sio.on('leave', namespace='/test')
-def leave(sid, message):
-    sio.leave_room(sid, message['room'], namespace='/test')
-    sio.emit('my response', {'data': 'Left room: ' + message['room']},
-             room=sid, namespace='/test')
-
-
-@sio.on('close room', namespace='/test')
-def close(sid, message):
-    sio.emit('my response',
-             {'data': 'Room ' + message['room'] + ' is closing.'},
-             room=message['room'], namespace='/test')
-    sio.close_room(message['room'], namespace='/test')
-
-
-@sio.on('my room event', namespace='/test')
-def send_room_message(sid, message):
-    sio.emit('my response', {'data': message['data']}, room=message['room'],
-             namespace='/test')
-
-
-@sio.on('disconnect request', namespace='/test')
-def disconnect_request(sid):
-    sio.disconnect(sid, namespace='/test')
-
-
-@sio.on('connect', namespace='/test')
+@sio.on('connect', namespace='/proc')
 def test_connect(sid, environ):
     sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
              namespace='/test')
 
 
-@sio.on('disconnect', namespace='/test')
+@sio.on('disconnect', namespace='/proc')
 def test_disconnect(sid):
     print('Client disconnected')
 
@@ -156,6 +121,11 @@ def processData():
         combinedData['Torque'] = combinedData['Force'] * (torqueArm*0.0393701)
     
     combinedData.to_csv('processed-data.csv')
+
+    #PASS FILE LOCATION
+    sio.emit('proc-finished', {'data': message['data']}, room=sid,
+            namespace='/proc')
+    
 
 if __name__ == '__main__':
     app.run(threaded=True)
